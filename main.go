c8048eb0558fbefecb1d8d8d90e62511cbd29996
rainrambler <wanganyu@outlook.com>
2021-06-20 17:11:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 // GoParser project main.go
 package main
 
-func exdemo(a, b int) (int, int) {
-	c := a
-	a = b
-	b = c
-	return a, b
-}
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	ParseGoSrc("demo.go.txt")
+	//ParseGoSrc("demo.go.txt")
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <path>\n", os.Args[0])
+		return
+	}
+	scanSrcDir(os.Args[1])
+
+	fmt.Printf("INFO: Total Go files: %d\n", totalFiles)
 }
